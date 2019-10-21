841766f03bf23db4ef16c21beb03c6ceb81c3012
Alex Thompson <aptbosox@gmail.com>
2019-10-21 14:29:31 -0400

    Bumped version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exemel',
-    version='0.2.2',
+    version='0.2.3',
     description=(
         'Build XML documents easily and concisely using native Python data '
         'structures.'
