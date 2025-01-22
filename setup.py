bb3b69846b0b08589f29045a00fae5e7d8de5882
Alex Thompson <alex.thompson@semgrep.com>
2025-01-22 11:14:10 -0500

    Update setup file

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 from setuptools import setup
 
 setup(
-    name='exemel',
-    version='0.2.3',
+    name="exemel",
+    version="0.2.3",
     description=(
-        'Build XML documents easily and concisely using native Python data '
-        'structures.'
+        "Build XML documents easily and concisely using native Python data "
+        "structures."
     ),
-    url='https://github.com/aptbosox/exemel',
-    author='Alexander Thompson',
-    author_email='aptbosox@gmail.com',
-    license='MIT',
+    url="https://github.com/apt-itude/exemel",
+    author="Alexander Thompson",
+    author_email="thompson.p.alex@gmail.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     py_modules=[
-        'exemel'
+        "exemel",
     ],
     install_requires=[
-        'future>=0.15.2',
-        'lxml>=3.0'
-    ]
+        "future>=0.15.2",
+        "lxml>=3.0",
+    ],
 )
